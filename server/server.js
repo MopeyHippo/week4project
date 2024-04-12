174930c20a442243b571f899b5b2c5117943b3a0
MopeyHippo <connor.james94@gmail.com>
2024-04-12 10:01:33 +0100

    server side JS

--- server/server.js
+++ server/server.js
@@ -1,11 +1,30 @@
 import express from "express";
 const app = express();
+const bodyParser = require('body-parser');
 const port = 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(bodyParser.json());
+
+let messages = [];
+
+app.post('/api/messages', (req, res) => {
+  const { text } = req.body;
+  if (text) {
+    const newMessage = { id: messages.length + 1, text };
+    messages.push(newMessage);
+    res.status(201).json(newMessage);
+  } else {
+    res.status(400).json({ error: 'Message text is required' });
+  }
 });
 
+app.get('/api/messages', (req, res) => {
+  res.json(messages);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
