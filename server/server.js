7733e60a340dcec755504d7af313329a243b0845
MopeyHippo <connor.james94@gmail.com>
2024-04-12 12:53:08 +0100

    work now plox

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser"
-import { Database } from 'better-sqlite3'; // Import Database class from better-sqlite3
-import Message from './models/message.js';
+import Database  from 'better-sqlite3'; // Import Database class from better-sqlite3
+import cors from "cors"
 
-const app = express();
+app.use(express());
+app.use(cors());
 const port = 8080;
 
 
